e4becd962e7f69940354d41915db85ca7d5b5959
Schenikova Snezhaa <snezha_00_97@mail.ru>
2024-12-29 17:23:14 +0300

    realized everyday's message of Internet balance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 import telebot
+import schedule
 import os
 
 myBot = telebot.TeleBot(os.environ.get("TOKEN"))
@@ -8,5 +9,28 @@ myBot = telebot.TeleBot(os.environ.get("TOKEN"))
 @myBot.message_handler(commands=['start'])
 def startMessage(message):
     myBot.send_message(message.chat.id, "Hello, world!")
+
+def get_stat():
+    everyDayCost = os.environ.get("EVERYDAYCOST")
+    startValue = os.environ.get("STARTVALUE")
+    currentValue = startValue - everyDayCost
+    os.environ["STARTVALUE"] = currentValue
+    return str(currentValue)
+
+
+def send_stat():
+    currentStat = get_stat()
+    message = 'На интернет счете сегодня: {currentStat} р.'
+    if currentStat < 100:
+        message+= 'Пора класть деньги!' 
+        
+    myBot.send_message(chat_id, message)
+
+
+schedule.every().day.at('17:30').do(send_stat)
+
+while True:
+    schedule.run_pending()
+    time.sleep(5)
 
 myBot.infinity_polling()
