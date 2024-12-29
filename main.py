b271e5b7bcc830a6a81f6c90203ddafc1a56f89b
Schenikova Snezhaa <snezha_00_97@mail.ru>
2024-12-29 21:08:07 +0300

    realized everyday's message of Internet balance

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import os
 import logging
 
 message_time = os.environ.get("TIME")
+CHANNEL_NAME = '@Snezzzz_bot'
 
 # Enable logging
 logging.basicConfig(
@@ -42,7 +43,7 @@ def send_stat():
     if float(currentStat) < 100.0:
         message+= 'Пора класть деньги!' 
         
-    myBot.send_message(message.chat_id, message)
+    myBot.send_message(CHANNEL_NAME, message)
 
 
 schedule.every(5).seconds.do(send_stat)  
