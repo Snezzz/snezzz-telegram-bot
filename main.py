8dc454b2049debd23f595fe0d3acb175fc236b04
Schenikova Snezhaa <snezha_00_97@mail.ru>
2025-01-14 16:06:32 +0300

    added actions for tasks collection

--- main.py
+++ main.py
@@ -53,10 +53,6 @@ def createTasksCollection(message):
         except OSError as err:
          myBot.send_message(message.chat.id, f"Ошибка при создании коллекции {err=}")
 
-@bot.message_handler(func=lambda m: True)
-def echo_all(message):
-	bot.reply_to(message, message.text)
-
 def createTask(message):
     arrTasks = []
     for task in message.text.split("\n"):
